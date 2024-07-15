bbee20b7a5a35f850711d41358988926fd77038d
Yeuoly <admin@srmxy.cn>
2024-07-15 15:36:21 +0800

    fix

--- python/dify_plugin/utils/io_writer.py
+++ python/dify_plugin/utils/io_writer.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 from pydantic import BaseModel
 
-from ..core.runtime.entities.message import SessionMessage
+from ..core.runtime.entities.plugin.message import SessionMessage
 
 class Event(Enum):
         LOG = 'log'
